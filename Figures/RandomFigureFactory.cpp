a426cca39cb47dd54a98ed629775211c2d21d8c0
MarioTonchev <mariotonchev500@gmail.com>
2025-11-15 18:47:45 +0200

    fixing triangle inequality when random triangle is generated

--- Figures/RandomFigureFactory.cpp
+++ Figures/RandomFigureFactory.cpp
@@ -5,24 +5,35 @@ std::unique_ptr<Figure> RandomFigureFactory::create() {
 
 	switch (type)
 	{
-	case 0:
-		double a = 1 + rand() % 20;
-		double b = 1 + rand() % 20;
-		double c = 1 + rand() % 20;
-		return  std::make_unique<Triangle>(a, b, c);
+		case 0:
+		{
+			double a = 1 + rand() % 20;
+			double b = 1 + rand() % 20;
+			double c = 1 + rand() % 20;
 
-		break;
-	case 1:
-		double w = 1 + rand() % 20;
-		double h = 1 + rand() % 20;
-		return std::make_unique<Rectangle>(w, h);
+			//we do this to fix triangle inequality if there is any
+			while (!(a + b > c && a + c > b && b + c > a))
+			{
+				a = 1 + rand() % 20;
+				b = 1 + rand() % 20;
+				c = 1 + rand() % 20;
+			}
 
-		break;
-	case 2:
-		double r = 1 + rand() % 20;
-		return std::make_unique<Circle>(r);
+			return std::make_unique<Triangle>(a, b, c);
+		}
+		case 1:
+		{
+			double w = 1 + rand() % 20;
+			double h = 1 + rand() % 20;
+			return std::make_unique<Rectangle>(w, h);
+		}
+		case 2:
+		{
+			double r = 1 + rand() % 20;
+			return std::make_unique<Circle>(r);
 
-		break;
+			break;
+	}
 	}
 
 	return nullptr;
