6adc35b83c1209cabd875209091186f19cf00848
MarioTonchev <mariotonchev500@gmail.com>
2025-11-16 22:56:37 +0200

    modifying random figure generation logic

--- Figures/RandomFigureFactory.cpp
+++ Figures/RandomFigureFactory.cpp
@@ -1,35 +1,44 @@
 #include "RandomFigureFactory.h"
 
+static double randomDouble(double min, double max) {
+	double fractionalPart = (double)rand() / RAND_MAX;
+
+	return min + fractionalPart * (max - min);
+}
+
 std::unique_ptr<Figure> RandomFigureFactory::create() {
 	int type = rand() % 3;
+
+	const double min = 1.0;
+	const double max = 10000.0;
+
 
 	switch (type)
 	{
 		case 0:
 		{
-			double a = 1 + rand() % 20;
-			double b = 1 + rand() % 20;
-			double c = 1 + rand() % 20;
+			double a = randomDouble(min, max);
+			double b = randomDouble(min, max);
+			double c = randomDouble(min, max);
 
-			//we do this to fix triangle inequality if there is any
 			while (!(a + b > c && a + c > b && b + c > a))
 			{
-				a = 1 + rand() % 20;
-				b = 1 + rand() % 20;
-				c = 1 + rand() % 20;
+				a = randomDouble(min, max);
+				b = randomDouble(min, max);
+				c = randomDouble(min, max);
 			}
 
 			return std::make_unique<Triangle>(a, b, c);
 		}
 		case 1:
 		{
-			double w = 1 + rand() % 20;
-			double h = 1 + rand() % 20;
+			double w = randomDouble(min, max);
+			double h = randomDouble(min, max);
 			return std::make_unique<Rectangle>(w, h);
 		}
 		case 2:
 		{
-			double r = 1 + rand() % 20;
+			double r = randomDouble(min, max);
 			return std::make_unique<Circle>(r);
 
 			break;
