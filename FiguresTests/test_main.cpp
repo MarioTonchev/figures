5e0c215c2a425a3698a769792a95d2ddb7cfdae3
MarioTonchev <mariotonchev500@gmail.com>
2025-11-19 17:30:30 +0200

    registering figure types before running tests

--- FiguresTests/test_main.cpp
+++ FiguresTests/test_main.cpp
@@ -1,2 +1,9 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch_amalgamated.hpp"
+#include "RegisterFigures.h"
+
+int main(int argc, char* argv[])
+{
+    registerAllFigures();
+    return Catch::Session().run(argc, argv);
+}
